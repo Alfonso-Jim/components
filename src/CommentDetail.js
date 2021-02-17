a10415d693c4d74bbb74537579849ae587408b1d
Alfonso Jim <alfonsojim1897@gmail.com>
2021-02-17 11:19:02 +0100

    update

---
+++ src/CommentDetail.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CommentDetail = () => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.image()} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Growling Brostion
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 5:00pm</span>
+        </div>
+        <div className="text">Nice postio brolio!</div>
+      </div>
+    </div>
+  );
+};
