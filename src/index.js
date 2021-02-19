e31ef208f7d476d38f7fa08f120486f9a1e59e25
Alfonso Jim <alfonsojim1897@gmail.com>
2021-02-19 12:13:31 +0100

    made CommentDetails.js available for app.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Growling Brostion
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 5:00pm</span>
-          </div>
-          <div className="text">Nice postio brolio!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Growling Brostion
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 5:00pm</span>
-          </div>
-          <div className="text">Nice postio brolio!</div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
